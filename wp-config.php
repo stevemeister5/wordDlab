1152497f0e0741b3c29bd97e8ac295a71f127363
rubberDuckie <stephenreubenm@gmail.com>
2020-04-27 16:39:56 +0300

    Http Settings test

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,15 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define("WP_SITEURL", "http://" . $_SERVER["HTTP_HOST"] . "/wp/");
+define("WP_HOME", "http://" . $_SERVER["HTTP_HOST"]);
+
+define("FORCE_SSL_LOGIN", getenv("FORCE_SSL_LOGIN") == "true");
+define("FORCE_SSL_ADMIN", getenv("FORCE_SSL_ADMIN") == "true");
+if ($_SERVER["HTTP_X_FORWARDED_PROTO"] == "https")
+	  $_SERVER["HTTPS"] = "on";
+
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
